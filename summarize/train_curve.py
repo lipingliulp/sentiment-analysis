14b4fb6c7fb7a2f29b54e95fd87e3ac085245a6d
Liping Liu <liping.liulp@gmail.com>
2016-12-19 14:20:56 -0500

    compare training curve

--- summarize/train_curve.py
+++ summarize/train_curve.py
@@ -8,31 +8,57 @@ sys.path.append('../util/')
 sys.path.append('../word2vec/')
 from util import config_to_name
 
+import matplotlib
 import matplotlib.pyplot as plt
 from matplotlib.legend_handler import HandlerLine2D
 
 data_path = '../data/restaurant/'
 
 # load model
-config = dict(use_sideinfo=True, K=128, max_iter=300000, half_window=1, reg_weight=1.0, num_neg=50)
-mfile = config_to_name(config) + '.pkl'
-train = pickle.load(open(data_path + 'splits/' + mfile, "rb"))
-logg_side = train['logg']
-step, loss = zip(*logg_side)
-print(len(step))
-index = range(130, len(step) - 2)
-plt.ylim(2.5, 4)
-plt.xlim(index[0] * 2000, index[-1] * 2000)
-line1, = plt.plot([step[i] for i in index], [loss[i] for i in index], color='red', label='w/ context attributes')
+config0 = dict(use_sideinfo=False, K=64, max_iter=300000, half_window=1, reg_weight=0.01, num_neg=500, negpos_ratio=1000, exposure=True)
+config1 = dict(use_sideinfo=False, K=64, max_iter=300000, half_window=1, reg_weight=0.01, num_neg=500, negpos_ratio=1000, exposure=True, cont_train=True)
+config2 = dict(use_sideinfo=True, K=64, max_iter=300000, half_window=1, reg_weight=0.01, num_neg=500, negpos_ratio=1000, exposure=True, cont_train=True)
 
-config = dict(use_sideinfo=False, K=128, max_iter=300000, half_window=1, reg_weight=1.0, num_neg=50)
-mfile = config_to_name(config) + '.pkl'
-train = pickle.load(open(data_path + 'splits/' + mfile, "rb"))
-logg = train['logg']
-step, loss = zip(*logg)
-print(loss)
-line2, = plt.plot([step[i] for i in index], [loss[i] for i in index], color='blue', label='w/o context attributes')
+mfile = config_to_name(config0) + '.pkl'
+loader = pickle.load(open(data_path + 'splits/' + mfile, "rb"))
+logg_exp = loader['logg']
 
+mfile = config_to_name(config1) + '.pkl'
+loader = pickle.load(open(data_path + 'splits/' + mfile, "rb"))
+logg_exp1 = loader['logg']
+
+mfile = config_to_name(config2) + '.pkl'
+loader = pickle.load(open(data_path + 'splits/' + mfile, "rb"))
+logg_side = loader['logg']
+
+step_side = range(302000, 600000, 2000) + [600000]
+loss_side = []
+loss_exp1 = []
+for i in xrange(1, 151):
+    loss_side.append(sum(logg_side[(i - 1) * 2000 : i * 2000]) / 2000)
+    loss_exp1.append(sum(logg_exp1[(i - 1) * 2000 : i * 2000]) / 2000)
+
+step_exp, loss_exp = zip(*logg_exp)
+step_exp = list(step_exp)
+loss_exp = list(loss_exp)
+step_exp[-1] = 300000
+
+step = step_exp + step_side
+loss = loss_exp + loss_exp1
+
+
+font = {'size' : 28}
+matplotlib.rc('font', **font)
+
+plt.ylim(5, 8)
+plt.xlim(0, 600000)
+index = range(0, 301)
+line1, = plt.plot(step, loss, color='red', label='w/o feats')
+line2, = plt.plot(step_side, loss_side, color='blue', label='w/ feats')
+
+plt.title('Negative log-likelihood of exposure model with and without features')
+plt.ylabel('negative log-likelihood')
+plt.xlabel('training mini-batches')
 plt.legend()
 plt.show()
 
