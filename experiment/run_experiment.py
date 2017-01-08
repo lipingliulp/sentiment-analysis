89d3f30eb225d70a3273c34cb10ff41d46d81e02
Liping Liu <liping.liulp@gmail.com>
2017-01-07 19:22:17 -0500

    minor changes to run_experiment

--- experiment/run_experiment.py
+++ experiment/run_experiment.py
@@ -7,7 +7,7 @@ from  movie_experiment import movie_experiment
 if __name__ == '__main__':
 
     #config = dict(use_sideinfo=False, K=16, max_iter=800000, half_window=1, reg_weight=0.01, num_neg=500, negpos_ratio=1000, exposure=False, cont_train=True) 
-    config = dict(use_sideinfo=False, K=64, max_iter=800000, reg_weight=0.01, exposure=False, cont_train=False)
+    config = dict(use_sideinfo=True, K=32, max_iter=100000, reg_weight=0.01, exposure=True, cont_train=False, sample_ratio=0.4)
    
     for iarg in xrange(1, len(sys.argv)):
         arg = sys.argv[iarg]
