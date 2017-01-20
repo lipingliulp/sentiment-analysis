4d9c4dc530a12f1cac9342f193a01c4421e933b3
lipingliulp <liping.liulp@gmail.com>
2017-01-19 20:43:33 -0500

    99 are treated as nan-s and filled with typical values

--- prepare_data/market/extract_user_feature.py
+++ prepare_data/market/extract_user_feature.py
@@ -30,6 +30,9 @@ for key in header:
     if key not in keeper:
         del table[key] 
 
+table.replace(99, np.nan, True)
+print(np.isnan(table).sum())
+
 # setting missing values to the most typical one
 flag = np.isnan(table['MICROWAVE'])
 table.loc[flag, 'MICROWAVE'] = 0
@@ -50,5 +53,37 @@ table.loc[flag, 'MALE_SMOKE'] = 0
 flag = np.isnan(table['Combined_Pre-Tax_Income_of_HH'])
 table.loc[flag, 'Combined_Pre-Tax_Income_of_HH'] = table['Combined_Pre-Tax_Income_of_HH'].mean()
 
-table.to_csv('../../data/market/user_feature.csv', index=False)
+key = 'Education_Level_Reached_by_Male_HH'
+flag = np.isnan(table[key])
+table.loc[flag, key] = table[key].mean()
+
+key = 'Education_Level_Reached_by_Female_HH'
+flag = np.isnan(table[key])
+table.loc[flag, key] = table[key].mean()
+
+key = 'MALE_HOUR'
+flag = np.isnan(table[key])
+table.loc[flag, key] = table[key].mean()
+
+key = 'HH_LANG'
+flag = np.isnan(table[key])
+table.loc[flag, key] = 0
+
+flag = table[key] > 1 
+table.loc[flag, key] = 2
+
+key = 'Female_Working_Hour_Code'
+flag = np.isnan(table[key])
+table.loc[flag, key] = table[key].mean()
+
+
+key = 'Household_Head_Race'
+flag = np.isnan(table[key])
+table.loc[flag, key] = 1
+flag = table[key] != 1
+table.loc[flag, key] = 0
+
+print(np.isnan(table).sum())
+
+table.to_csv(datapath + 'user_feature.csv', index=False)
 
