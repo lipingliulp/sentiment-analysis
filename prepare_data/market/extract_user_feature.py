0f4b82fbdb649b04f377ea325c65945c6baaae2c
lipingliulp <liping.liulp@gmail.com>
2017-01-19 20:06:18 -0500

    extract features from market dataset

--- prepare_data/market/extract_user_feature.py
+++ prepare_data/market/extract_user_feature.py
@@ -10,22 +10,45 @@ def save_data(savename, obsmat, obscov, index):
     pkl.dump(reviews, open(savename, 'wb'))
 
 # load from pemb data
-datapath = os.path.expanduser('~/storage/sa-data/market/')
+datapath = os.path.expanduser('../../data/market/')
 data = np.genfromtxt(datapath + 'panelists12.csv',
                       delimiter='\t', missing_values='?') 
 
 
 
 
-header = ['Panelist_ID', 'Panelist_Type', 'Combined_Pre-Tax_Income_of_HH', 'Family_Size', 'Household_Head_Race', 'Type_of_Residential_Possession', 'COUNTY', 'Age_Group_Applied_to_Household_Head', 'Education_Level_Reached_by_Household_Head', 'Occupation_Code_of_Household_Head', 'Age_Group_Applied_to_Male_HH', 'Education_Level_Reached_by_Male_HH', 'Occupation_Code_of_Male_HH', 'MALE_HOUR', 'MALE_SMOKE', 'Age_Group_Applied_to_Female_HH', 'Education_Level_Reached_by_Female_HH', 'Occupation_Code_of_Female_HH', 'Female_Working_Hour_Code', 'FEM_SMOKE', 'Number_of_Dogs', 'Number_of_Cats', 'Children_Group_Code', 'Marital_Status_(WARNING:_This_may_be_flipped_for_years_8-11)', 'HH_LANG', 'ALL_TVS', 'CABL_TVS', 'Hispanic_Flag', 'HISP_CAT', 'RACE2', 'RACE3', 'MICROWAVE', 'device_type', 'ZIPCODE', 'FIPSCODE', 'market_based_upon_zipcode', 'IRI_Geography_Number', 'EXT_FACT']
+header = ['Panelist_ID', 'Panelist_Type', 'Combined_Pre-Tax_Income_of_HH', 'Family_Size', 'Household_Head_Race', 'Type_of_Residential_Possession', 'COUNTY', 'Age_Group_Applied_to_Household_Head', 'Education_Level_Reached_by_Household_Head', 'Occupation_Code_of_Household_Head', 'Age_Group_Applied_to_Male_HH', 'Education_Level_Reached_by_Male_HH', 'Occupation_Code_of_Male_HH', 'MALE_HOUR', 'MALE_SMOKE', 'Age_Group_Applied_to_Female_HH', 'Education_Level_Reached_by_Female_HH', 'Occupation_Code_of_Female_HH', 'Female_Working_Hour_Code', 'FEM_SMOKE', 'Number_of_Dogs', 'Number_of_Cats', 'Children_Group_Code', 'Marital_Status', 'HH_LANG', 'ALL_TVS', 'CABL_TVS', 'Hispanic_Flag', 'HISP_CAT', 'RACE2', 'RACE3', 'MICROWAVE', 'device_type', 'ZIPCODE', 'FIPSCODE', 'market_based_upon_zipcode', 'IRI_Geography_Number', 'EXT_FACT']
 
-data[data == 99] = np.nan
+#data[data == 99] = np.nan
 
-keeper = ['Panelist_ID', 'Combined_Pre-Tax_Income_of_HH', 'Family_Size', 'Household_Head_Race', 'Type_of_Residential_Possession', 'Age_Group_Applied_to_Household_Head', 'Education_Level_Reached_by_Household_Head', 'Occupation_Code_of_Household_Head', 'Age_Group_Applied_to_Male_HH', 'Education_Level_Reached_by_Male_HH', 'Occupation_Code_of_Male_HH', 'MALE_HOUR', 'MALE_SMOKE', 'Age_Group_Applied_to_Female_HH', 'Education_Level_Reached_by_Female_HH', 'Occupation_Code_of_Female_HH', 'Female_Working_Hour_Code', 'FEM_SMOKE', 'Number_of_Dogs', 'Number_of_Cats', 'Children_Group_Code', 'Marital_Status', 'HH_LANG', 'ALL_TVS', 'CABL_TVS', 'Hispanic_Flag', 'HISP_CAT', 'MICROWAVE', 'device_type']
+keeper = ['Panelist_ID', 'Combined_Pre-Tax_Income_of_HH', 'Family_Size', 'Household_Head_Race', 'Type_of_Residential_Possession', 'Age_Group_Applied_to_Male_HH', 'Education_Level_Reached_by_Male_HH', 'MALE_HOUR', 'MALE_SMOKE', 'Age_Group_Applied_to_Female_HH', 'Education_Level_Reached_by_Female_HH', 'Female_Working_Hour_Code', 'FEM_SMOKE', 'Number_of_Dogs', 'Number_of_Cats', 'Children_Group_Code', 'Marital_Status', 'HH_LANG', 'ALL_TVS', 'Hispanic_Flag', 'MICROWAVE']
 
 
 table = pandas.DataFrame(data, columns=header)
 
-sum(table.)
+for key in header:
+    if key not in keeper:
+        del table[key] 
 
-print(table)
+# setting missing values to the most typical one
+flag = np.isnan(table['MICROWAVE'])
+table.loc[flag, 'MICROWAVE'] = 0
+
+flag = np.isnan(table['Hispanic_Flag'])
+table.loc[flag, 'Hispanic_Flag'] = 2
+
+column = table['ALL_TVS']
+table.loc[np.isnan(column), 'ALL_TVS'] = 1
+table.loc[column > 4, 'ALL_TVS'] = 5
+
+flag = np.isnan(table['FEM_SMOKE'])
+table.loc[flag, 'FEM_SMOKE'] = 0
+
+flag = np.isnan(table['MALE_SMOKE'])
+table.loc[flag, 'MALE_SMOKE'] = 0
+
+flag = np.isnan(table['Combined_Pre-Tax_Income_of_HH'])
+table.loc[flag, 'Combined_Pre-Tax_Income_of_HH'] = table['Combined_Pre-Tax_Income_of_HH'].mean()
+
+table.to_csv('../../data/market/user_feature.csv', index=False)
+
